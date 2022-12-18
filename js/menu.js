d7f922a875b1922edca89aed78b2147c2a818acb
Zlata Filcheva <fzlata6@gmail.com>
2022-12-18 22:19:23 +0200

    menu was corrected

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const menuEl = document.getElementById("menu");
+const expandMenuButton = document.getElementById("expandMenu");
 
 const menu = [
     {
@@ -40,7 +41,7 @@ const menu = [
 const generatedMenu = menu
     .map(({link, text}, index) => {
         return `
-            <div class=${index > 3 ? "hidden" : ""}> 
+            <div class='${index > 3 ? "hidden menu-el" : "menu-el"}'> 
                 <a href=/main/Жанры/${link}>${text}</a>
             </div>
         `;
@@ -49,3 +50,22 @@ const generatedMenu = menu
 
 menuEl.innerHTML = generatedMenu;
 
+expandMenuButton.addEventListener("click", () => {
+    const menuElements = document.getElementsByClassName("menu-el");
+    const menuElementsArray = [...menuElements];
+
+    menuElementsArray.forEach(element => {
+        if (element.className.includes("hidden")){
+            element.classList.remove("hidden");
+            element.classList.add("opened");
+
+            return;
+        }
+
+        if (element.className.includes("opened")) {
+            element.classList.remove("opened");
+            element.classList.add("hidden");
+        }
+    });
+
+});
