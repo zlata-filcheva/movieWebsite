167fa340d7cf36162108753630006fd41e3bf3c8
zlata-filcheva <109035310+zlata-filcheva@users.noreply.github.com>
2022-12-20 16:21:16 +0200

    2 correct menu (#2)
    * 2 correct menu
    
    * menu was corrected

---
+++ js/SiteMenu.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const expandMenuButton = document.getElementById("expandMenu");
+
+(async () => {
+    const menuEl = document.getElementById("menu");
+    const {menu} = await import("./constants/menu.js");
+
+    menuEl.innerHTML = menu
+        .map(({link, name}, index) => {
+            return `
+            <div class='${index > 3 ? "hidden siteMenu-el" : "siteMenu-el"}'> 
+                <a href=/main/Жанры/${link}>${name}</a>
+            </div>
+        `;
+        })
+        .join(``);
+})();
+
+expandMenuButton.addEventListener("click", () => {
+    const menuElements = document.getElementsByClassName("siteMenu-el");
+    const menuElementsArray = [...menuElements];
+
+    menuElementsArray.forEach(element => {
+        if (element.className.includes("hidden")){
+            element.classList.remove("hidden");
+            element.classList.add("opened");
+
+            return;
+        }
+
+        if (element.className.includes("opened")) {
+            element.classList.remove("opened");
+            element.classList.add("hidden");
+        }
+    });
+
+});
