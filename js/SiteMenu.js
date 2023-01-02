33c774df15fbc658c7f8524eea95a135ebd1e211
zlata-filcheva <109035310+zlata-filcheva@users.noreply.github.com>
2023-01-02 21:17:41 +0200

    8 new pages (#10)
    * some changes
    
    * some changes
    
    * some changes
    
    * some changes
    
    * some changes
    
    * some changes
    
    * some changes
    
    * some changes
    
    * some changes
    
    * some changes
    
    * some changes
    
    * some changes

--- js/SiteMenu.js
+++ js/SiteMenu.js
@@ -10,7 +10,7 @@ const expandMenuButton = document.getElementById("expandMenu");
         .map(({link, name}, index) => {
             return `
             <div class='${index > 3 ? "hidden siteMenu-el" : "siteMenu-el"}'> 
-                <a href="/movieWebsite/main/Жанры/${link}">${name}</a>
+                <a href=".././main/genres/${link}">${name}</a>
             </div>
         `;
         })
